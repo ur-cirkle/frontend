803d1b5272628f9711a2ecade279709b235942e6
Heet Vakharia <heet1476@gmail.com>
2021-06-01 23:11:17 +0530

    Fixed bugs

--- src/Components/ImageEditor/AddPostForm.component.tsx
+++ src/Components/ImageEditor/AddPostForm.component.tsx
@@ -1,3 +1,12 @@
 import React, { useReducer } from "react";
 import { PostForm } from "../../Interfaces/ImageEditor.intrefaces";
+export interface NavbarProps {}
 
+const Navbar: React.FC<NavbarProps> = () => {
+  
+    return <div className="">
+      Navbar
+  </div>;
+};
+
+export default Navbar;
