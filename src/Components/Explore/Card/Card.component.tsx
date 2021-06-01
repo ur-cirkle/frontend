43accd64bade5a0e85628fc7266bcc09dc2412ef
Priyank Rana <priyank223017@gmail.com>
2021-06-02 02:22:27 +0530

    Further CSS done in Explore Cards

--- src/Components/Explore/Card/Card.component.tsx
+++ src/Components/Explore/Card/Card.component.tsx
@@ -1,34 +1,36 @@
 import React from "react";
 import { CardProps } from "../../../Interfaces/Card.interfaces.js";
-import CardStyle from './Card.styles';
+import {CardWrapper} from './Card.styles';
+import {FirstName} from './Card.styles';
+import {SubmitButton} from './Card.styles';
+import {Description} from './Card.styles';
+import {Connections} from './Card.styles';
 
 const Card: React.FC<CardProps> = ({ cardRef, cardUser, index }) => {
     let imgUrl = "http://placeimg.com/100/100/animals";
     return (
-      <CardStyle>
-        <div className="Card" ref={cardRef}>
+        <CardWrapper className="Card" ref={cardRef}>
           <img
             src={cardUser.picture.large}
             alt=""
             className="Image"
             style={{ borderRadius: "50%" }}
           />
-          <h1>
+          <FirstName>
             {cardUser.name.first}
-          </h1>
-          <p className="">{Math.floor(Math.random() * 5000 + 1)} Connections</p>
-          <button>Connect</button>
-          <p>
+          </FirstName>
+          <Connections className="">{Math.floor(Math.random() * 5000 + 1)} </Connections>
+          <SubmitButton>Connect</SubmitButton>
+          <Description>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis,
             doloribus voluptatum. Saepe architecto unde dolorum est, ratione
-          </p>
+          </Description>
           <div className="card_imgs">
             <img src={imgUrl} alt="" />
             <img src={imgUrl} alt="" />
             <img src={imgUrl} alt="" />
           </div>
-        </div>
-      </CardStyle>
+        </CardWrapper>
     );
 };
 
--- src/Components/ImageEditor/AddPostForm.component.tsx
+++ src/Components/ImageEditor/AddPostForm.component.tsx
@@ -1,5 +1,4 @@
 import React, { useReducer } from "react";
-import { PostForm } from "../../Interfaces/ImageEditor.intrefaces";
 export interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
